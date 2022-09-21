acbaf28890df8131204da8db3e8279e6b65dae78
ijritchey <ijritchey@hotmail.com>
2022-09-20 22:43:09 -0700

    updated navbar, profile, css style

--- controllers/conspiracy.js
+++ controllers/conspiracy.js
@@ -2,14 +2,29 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// conspiracy index get route
+router.get('/', (req, res) => {
+    db.conspiracy.findAll()
+    .then((conspiracy) => {
+        res.render('conspiracy/index', {conspiracies: conspiracy})
+    })
+    .catch((error => {
+        res.status(400).render('main/404')
+    }))
+});
+
+
 // conspiracy post route
 router.post('/', (req, res) => {
-    db.article.create({
-        name: req.body.name,
-        description: req.body.description
+    db.conspiracy.create({
+        userId: 1,
+        title: req.body.title,
+        description: req.body.description,
+        rating: 0,
+        isLive: false
     })
         .then((post) => {
-            res.redirect('/')
+            res.redirect('/profile')
         })
         .catch((error) => {
             res.status(400).render('main/404')
@@ -17,14 +32,9 @@ router.post('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-    db.user_conciracies.findAll()
-        .then((authors) => {
-            res.render('conspiracy/new', { conspiracy: conspiracy })
-        })
-        .catch((error) => {
-            res.status(400).render('main/404')
-        })
-})
+    res.render('conspiracy/new')
+});
+    
 
 
 
