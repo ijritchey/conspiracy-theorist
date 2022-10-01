35af2302ab9825ccd36d12ff1abac88098d3faf5
ijritchey <ijritchey@hotmail.com>
2022-10-01 10:34:47 -0700

    chore: setting up heroku

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('*', (req, res) => {
   res.render('main/404')
 })
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
